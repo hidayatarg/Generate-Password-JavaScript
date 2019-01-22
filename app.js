c5a7e2195ac44618020c6146d5633d1fcadcadb0
Hidayat arghandabi <hidayatarg@gmail.com>
2019-01-22 14:01:43 +0300

    Generate UserName Automatically

--- app.js
+++ app.js
@@ -16,3 +16,18 @@ function generate() {
     myform.row_password.value = randomPassword(20);
    
 }
+
+function generateUsername() {
+    // Setting length from the Html any element
+    //myform.row_password.value = randomPassword(myform.length.value);
+
+    // Here you can set the password lenght manually
+   genform.row_username.value = randomUsername();
+   console.log(genform.row_username.value);
+
+}
+function randomUsername(){
+    let firstName = genform.update_firstname.value[0];
+    let lastName = genform.update_lastname.value;
+    return lastName+firstName;
+}
